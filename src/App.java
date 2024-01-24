00de05ae7a9580e2a7b66b10d4a2a7e5fa565907
DeviSriAditya18 <devisriaditya17@gmail.com>
2024-01-24 18:40:26 +0530

    first commit

---
+++ src/App.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int fact=1;
+        for(int i=1;i<=n;i++) fact*=i;
+        System.out.print("Factorial is:"+fact);
+    }
+}
